21b470396f767328208fb10dd32b14716df90003
milnas <ivo.a.f.silva@gmail.com>
2018-02-19 11:27:13 +0000

    First Commit - Project Setup

---
+++ app/src/main/java/com/ivoafsilva/floowchallenge/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ivoafsilva.floowchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
